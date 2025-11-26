22edb897b018eeb4c8a066a61cc43726407096ec
Bluem <140798194+JohnCarloSabenorio@users.noreply.github.com>
2025-11-26 13:54:57 +0800

    added footer margin

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="bg-gray-600 p-3 text-white">
+    <div className="bg-gray-600 p-3 text-white mt-3">
       <p className="text-center">© 2025 JC Sabenorio. All Rights Reserved.</p>
     </div>
   );
